e1f19342ceccb8ee7e6ab814f851c83e19400eed
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-25 16:21:36 +0530

    init-project

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,18 +3,13 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+  appName?: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+export const Button = ({ appName }: ButtonProps) => {
+  return ( <div className="border-2">
+    tech
+  </div>
+        
   );
 };
